35f4600c47f7fa5b3304899dcdf2d9495a5aa0a7
Bigmotech <momowebpages@gmail.com>
2021-12-12 11:50:22 -0600

    Upload changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 import axios from 'axios';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="Changed">
       <header className="App-header">
-       <button name='Hello'></button>
+       <button onClick={() => {
+         console.log("ehlo there")
+         var obj = document.getElementById("Changed")
+         obj.style.backgroundColor = "#FFD712"
+       }} className="ChangeMe">Hello There you son of a bitch</button>
       </header>
     </div>
   );
